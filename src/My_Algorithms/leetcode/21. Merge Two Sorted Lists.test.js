dbf07c970a19023370be01cd7febf5c178e67466
Denys Davydov <denysdavydov83@gmail.com>
2022-02-12 21:11:59 +0200

    leetcode: 21. Merge Two Sorted Lists

---
+++ src/My_Algorithms/leetcode/21. Merge Two Sorted Lists.test.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+const mergeTwoLists = function (list1, list2) {
+  const dummy = {
+    val: -1,
+    next: null,
+  };
+
+  let head = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      head.next = list1;
+      list1 = list1.next;
+    } else {
+      head.next = list2;
+      list2 = list2.next;
+    }
+    head = head.next;
+  }
+
+  head.next = list1 || list2;
+  return dummy.next;
+};
