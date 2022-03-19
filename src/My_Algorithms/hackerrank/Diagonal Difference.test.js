0418c7950f813e8f6f7f94d5536e1bc0b397b24a
Denys Davydov <denysdavydov83@gmail.com>
2022-03-19 19:49:42 +0200

    hackerrank:Diagonal Difference

---
+++ src/My_Algorithms/hackerrank/Diagonal Difference.test.js
@@ -0,0 +1,29 @@
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+// initial solution
+function diagonalDifference_initial(arr) {
+  const diagonal1 = arr.map((row, idx) => row[idx]).reduce((a, b) => a + b, 0);
+  const diagonal2 = arr.map((row, idx) => row[row.length - idx - 1]).reduce((a, b) => a + b, 0);
+  return Math.abs(diagonal1 - diagonal2);
+}
+
+// enhanced solution
+function diagonalDifference(arr) {
+  return Math.abs(
+    arr.map((row, idx) => row[idx] - row[row.length - idx - 1]).reduce((a, b) => a + b, 0),
+  );
+}
+
+test('twoSum', () => {
+  const map1 = [[1, 2, 3], [4, 5, 6], [9, 8, 9]];
+  const map2 = [[11, 2, 4], [4, 5, 6], [10, 8, -12]];
+  expect(diagonalDifference(map1)).toBe(2);
+  expect(diagonalDifference(map2)).toBe(15);
+});
