3eba1fcae5a5730d7bd2278a719c902ffcc29635
Denys Davydov <denysdavydov83@gmail.com>
2022-06-15 13:15:57 +0200

    bigfrontend: 50. search element right before target with Binary Search(possible duplicate array)

---
+++ src/My_Algorithms/bigfrontend.dev/50. search element right before target with Binary Search(possible duplicate array).test.js
@@ -0,0 +1,24 @@
+// https://bigfrontend.dev/problem/search-element-right-before-target-with-Binary-Search-possible-duplicate-array
+
+/**
+ * @param {number[]} arr - ascending array with duplicates
+ * @param {number} target
+ * @return {number}
+ */
+function elementBefore(arr, target) {
+  let left = 0; let
+    right = arr.length - 1;
+
+  while (left < right) {
+    const mid = left + right >> 1;
+    if (arr[mid] >= target) right = mid;
+    else left = mid + 1;
+  }
+
+  return arr[left] === target ? arr[left - 1] : undefined;
+}
+
+test('elementBefore', () => {
+  expect(elementBefore([1, 2, 3, 4, 4, 4, 4, 100, 1000, 10000], 4)).toBe(3);
+  expect(elementBefore([1, 1, 1], 1)).toBe(undefined);
+});
