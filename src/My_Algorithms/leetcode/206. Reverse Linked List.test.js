eeb3a025365175246fa4d805c18674e763c6708e
Denys Davydov <denysdavydov83@gmail.com>
2022-07-11 13:28:18 +0200

    leetcode: 206. Reverse Linked List

---
+++ src/My_Algorithms/leetcode/206. Reverse Linked List.test.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+  let node = head;
+  let previous = null;
+
+  while (node) {
+    // save next or you lose it!!!
+    const save = node.next;
+    // reverse pointer
+    node.next = previous;
+    // increment previous to current node
+    previous = node;
+    // increment node to next node or null at end of list
+    node = save;
+  }
+  // return previous; // Change the list head !!!
+  return previous;
+};
