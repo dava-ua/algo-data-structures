c51c7a0cca15c3b46c97cfd559d1d09f9c943f60
Denys Davydov <denysdavydov83@gmail.com>
2022-11-04 16:28:55 +0100

    leetcode: 543. Diameter of Binary Tree

---
+++ src/My_Algorithms/leetcode/543. Diameter of Binary Tree.test.js
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// not my solution
+const diameterOfBinaryTree = function (root) {
+  let diameter = 0;
+
+  function dfs(node) {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    // update diameter at every node
+    diameter = Math.max(diameter, left + right);
+
+    // update the largest number of edge so far
+    return 1 + Math.max(left, right);
+  }
+
+  dfs(root);
+
+  return diameter;
+};
