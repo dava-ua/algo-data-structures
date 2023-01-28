bc31ccb2ca0a4d851aeaf257ed969db2f558bc23
Denys Davydov <denysdavydov83@gmail.com>
2023-01-28 11:48:26 +0200

    leetcode: 79. Word Search

---
+++ src/My_Algorithms/leetcode/medium/backtracking/79. Word Search.test.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/word-search/
+
+/**
+ * @param {string[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+const exist = function (board, word) {
+  const ROW_NUM = board.length;
+  const COL_NUM = board[0].length;
+
+  function callDFS(r, c, idx) {
+    if (word.length === idx) return true;
+    if (r >= ROW_NUM || r < 0 || board[r][c] !== word[idx]) return false;
+
+    board[r][c] = '#'; // mark as visited
+
+    if (callDFS(r + 1, c, idx + 1)
+      || callDFS(r - 1, c, idx + 1)
+      || callDFS(r, c + 1, idx + 1)
+      || callDFS(r, c - 1, idx + 1)) {
+      return true;
+    }
+
+    board[r][c] = word[idx]; // reset the board
+    return false;
+  }
+
+  for (let r = 0; r < ROW_NUM; r += 1) {
+    for (let c = 0; c < COL_NUM; c += 1) {
+      if (board[r][c] === word[0] && callDFS(r, c, 0)) return true;
+    }
+  }
+
+  return false;
+};
